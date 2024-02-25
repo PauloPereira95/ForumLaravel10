19be8ebadd90448c5e9699af5bfe102d8c95dc8e
PaulPereira95 <paulohuawei95@gmail.com>
2024-02-25 18:51:57 +0000

    feat: Index page

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -9,26 +9,8 @@
 @include('admin/supports/partials/header' ,compact('supports'))
 @endsection
 {{-- Inside e link is the name of the route . view on web.php  --}}
-<a href="{{ route('supports.create') }}">Fazer Pedido de Supporte</a>
-<table style="text-align:right;">
-    <thead>
-        <th>Assunto</th>
-        <th>Status</th>
-        <th>Descricao</th>
-        <th>Acoes</th>
-    </thead>
-    <tbody>
 
-        @foreach ($supports->items() as $support)
-        <tr>
-            <td>{{ $support->subject }}</td>
-            <td>{{ getStatusSupport($support->status) }}</td>
-            <td>{{ $support->body }}</td>
-            <td> <a href="{{ route('supports.show',$support->id) }}"> ></a></td>
-            <td><a href="{{ route('supports.edit',$support->id) }}">Editar</a></td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+@section('content')
+    @include('admin/supports/partials/content')
 <x-pagination :paginator="$supports" :appends="$filters"/>
 @endsection
